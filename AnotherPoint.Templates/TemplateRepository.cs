ff0ec094ba6ac93336c27dcca03d6aabe3a6e041
snowinmars <stuff4work@yandex.ru>
2017-03-04 22:13:26 +0400

    Refactored.

--- AnotherPoint.Templates/TemplateRepository.cs
+++ AnotherPoint.Templates/TemplateRepository.cs
@@ -12,53 +12,23 @@ namespace AnotherPoint.Templates
 {
 	public static class TemplateRepository
 	{
-		private static readonly IDictionary<TemplateType, string> NameFileBinding;
+		private static readonly DynamicViewBag dynamicViewBag;
 		private static readonly string RootFolder = Directory.GetCurrentDirectory();
+		private static IDictionary<TemplateType, string> nameFileBinding;
 		private static IRazorEngineService razorService;
 
 		static TemplateRepository()
 		{
 			dynamicViewBag = new DynamicViewBag();
 
-			TemplateRepository.NameFileBinding = new Dictionary<TemplateType, string>();
-
-			foreach (var enumName in Enum.GetNames(typeof(TemplateType)))
-			{
-				TemplateType templateType;
-				if (!Enum.TryParse(enumName, out templateType))
-				{
-					throw new InvalidOperationException($"Can't parse enum {nameof(TemplateType)}: string enum value is wrong: it's {enumName}");
-				}
-
-				NameFileBinding.Add(templateType, $"{Path.Combine(TemplateRepository.RootFolder, "dat", $"{enumName}.dat")}");
-			}
+			InitializeNameFileBinding();
 
 			TemplateRepository.SelfValidate();
-
-			var config = new TemplateServiceConfiguration
-			{
-				Language = Language.CSharp,
-#if DEBUG
-				Debug = true,
-#endif
-			};
+			TemplateServiceConfiguration config = GetDefaultConfig();
 
 			TemplateRepository.razorService = RazorEngineService.Create(config);
-
-			foreach (var pair in TemplateRepository.NameFileBinding)
-			{
-				ITemplateKey templateKey = new NameOnlyTemplateKey(pair.Key.AsString(),
-																	ResolveType.Layout,
-																	context: null);
-
-				ITemplateSource templateSource = new LoadedTemplateSource(File.ReadAllText(pair.Value), pair.Value);
-
-				TemplateRepository.razorService.AddTemplate(templateKey,
-															templateSource);
-			}
+			InitRazorEngineTemplates();
 		}
-
-		private static readonly DynamicViewBag dynamicViewBag;
 
 		public static string Compile(TemplateType template, object model)
 		{
@@ -76,9 +46,53 @@ namespace AnotherPoint.Templates
 			return v;
 		}
 
+		private static TemplateServiceConfiguration GetDefaultConfig()
+		{
+			var config = new TemplateServiceConfiguration
+			{
+				Language = Language.CSharp,
+#if DEBUG
+				Debug = true,
+#endif
+			};
+
+			return config;
+		}
+
+		private static void InitializeNameFileBinding()
+		{
+			TemplateRepository.nameFileBinding = new Dictionary<TemplateType, string>();
+
+			foreach (var enumName in Enum.GetNames(typeof(TemplateType)))
+			{
+				TemplateType templateType;
+				if (!Enum.TryParse(enumName, out templateType))
+				{
+					throw new InvalidOperationException($"Can't parse enum {nameof(TemplateType)}: string enum value is wrong: it's {enumName}");
+				}
+
+				nameFileBinding.Add(templateType, $"{Path.Combine(TemplateRepository.RootFolder, "dat", $"{enumName}.dat")}");
+			}
+		}
+
+		private static void InitRazorEngineTemplates()
+		{
+			foreach (var pair in TemplateRepository.nameFileBinding)
+			{
+				ITemplateKey templateKey = new NameOnlyTemplateKey(pair.Key.AsString(),
+																	ResolveType.Layout,
+																	context: null);
+
+				ITemplateSource templateSource = new LoadedTemplateSource(File.ReadAllText(pair.Value), pair.Value);
+
+				TemplateRepository.razorService.AddTemplate(templateKey,
+															templateSource);
+			}
+		}
+
 		private static void SelfValidate()
 		{
-			TemplateRepository.NameFileBinding
+			TemplateRepository.nameFileBinding
 				.Values
 				.Where(templatePath => !File.Exists(templatePath))
 				.ForEach(templatePath =>
